2c7ec9874e4aa7365a91b2b28060c9ab8f249642
niiodarteytt <niiodartey.tt@gmail.com>
2025-11-02 16:17:03 +0000

    delete native files

--- escrow-react-native/src/screens/Splash/SplashScreen.Native.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text, Pressable, Image, TextInput } from 'react-native';
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F9FAFB',
-  },
-});
